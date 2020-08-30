d2a2c8ab578a32825c486003e4f56a97063630e8
Zhuchkou Artsiom <zidane.from.france@gmail.com>
2020-08-30 20:24:18 +0300

    fix: add build

---
+++ build/precache-manifest.6b7c9f61eeca3fa6d14835af4fbbbae6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b368801523c76ed2e29d73adae03da6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a356cdb229e5e3761ecc",
+    "url": "/static/css/2.9131a3d6.chunk.css"
+  },
+  {
+    "revision": "988d814cc400bc9087fa",
+    "url": "/static/css/main.45df450d.chunk.css"
+  },
+  {
+    "revision": "a356cdb229e5e3761ecc",
+    "url": "/static/js/2.67f0a107.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.67f0a107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988d814cc400bc9087fa",
+    "url": "/static/js/main.9b2d104b.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/static/media/bird.06a46938.jpg"
+  },
+  {
+    "revision": "241eeef849fbbd3fdd6f7c54ac122151",
+    "url": "/static/media/correct-answer.241eeef8.mp3"
+  },
+  {
+    "revision": "262a6b7ba5eb99bab624e8573507ad18",
+    "url": "/static/media/incorrect-sound.262a6b7b.mp3"
+  },
+  {
+    "revision": "4f82cd73f3446ef1af7fa6d3a302ef3a",
+    "url": "/static/media/logo.4f82cd73.svg"
+  }
+]);
